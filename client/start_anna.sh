86b6745165fd74e508ca0422620cc6bd92fc1947
Chris Lewicki <chris@lewicki.dev>
2025-04-10 16:56:05 -0400

    make the startup script a little more useful

--- client/start_anna.sh
+++ client/start_anna.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # Replace with your real command to start the model
-ollama run mistral &
+ollama serve &
 
 sleep 5
 
+tailscale serve --http=11435 11434 &
+
 # Notify VPS
 curl -X POST http://anna-scaffold.snowy-hen.ts.net:5000/api/anna/status -d '{"online": true}' -H "Content-Type: application/json"
+
+trap 'curl -X POST http://anna-scaffold.snowy-hen.ts.net:5000/api/anna/status -d "{\"online\": false}" -H "Content-Type: application/json" && exit' SIGINT SIGTERM
